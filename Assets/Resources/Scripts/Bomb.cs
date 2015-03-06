0363f81c6a0236afe5643fcca7fbcaea2d282c75
GKona <gerikona@hotmail.com>
2015-03-06 16:52:44 -0500

    Dynoman Transfer

---
+++ Assets/Resources/Scripts/Bomb.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+
+
+	public float bombFuse = 2f;
+	public GameObject spawnExplosion;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		StartCoroutine("BombTimer");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	IEnumerator BombTimer (){
+
+		yield return new WaitForSeconds(bombFuse);
+		GameObject.Find("Player").GetComponent<SpawnBomb>().amount--;
+		GameObject.Destroy(this.gameObject);
+		Instantiate(spawnExplosion,transform.position, transform.rotation);
+
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x,transform.position.y,transform.position.z+1), transform.rotation);
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x,transform.position.y,transform.position.z+2), transform.rotation);
+
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x,transform.position.y,transform.position.z-1), transform.rotation);
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x,transform.position.y,transform.position.z-2), transform.rotation);
+
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x+1,transform.position.y,transform.position.z), transform.rotation);
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x+2,transform.position.y,transform.position.z), transform.rotation);
+
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x-1,transform.position.y,transform.position.z), transform.rotation);
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x-2,transform.position.y,transform.position.z), transform.rotation);
+
+
+	}
+
+
+
+}
---
+++ Assets/Resources/Scripts/p8.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class p8 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//Deny player movement if detects obstacle
+	void OnTriggerStay (Collider other){
+		if (other.gameObject.name == "Wall" || other.gameObject.name == "Cube1"){
+			GameObject.Find("Player").GetComponent<PlayerMovement>().canMove = false;
+		}
+	}
+
+	//Allow player movement otherwise
+	void OnTriggerExit (Collider other){
+		GameObject.Find("Player").GetComponent<PlayerMovement>().canMove = true;
+	}
+
+}
