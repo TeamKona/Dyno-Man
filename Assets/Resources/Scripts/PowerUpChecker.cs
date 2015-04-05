1984cefae9a43fb2d952dca55fabd541f38ee880
ryans3 <ryan.s3@hotmail.com>
2015-04-05 17:45:38 -0400

    #46
    decreased explosion radius to 1 unit for 5 seconds

--- Assets/Resources/Scripts/PowerUpChecker.cs
+++ Assets/Resources/Scripts/PowerUpChecker.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PowerUpChecker : MonoBehaviour {
 
 	public GameObject bomb;
+	public GameObject bombEx;
 
 	// Use this for initialization
 	void Start () {
@@ -43,9 +44,29 @@ public class PowerUpChecker : MonoBehaviour {
 
 		}
 
+		if(col.gameObject.name == "ExplosionReduction")
+		{
+			Debug.Log("Power Down Collected");
+			StartCoroutine("SmallExplosion");
+		}
+
 
 	}
+
 	
+	IEnumerator SmallExplosion (){
+		
+		bomb.GetComponent<Bomb>().expAreaNorm = 1;
+		
+		bombEx.GetComponent<BombEx>().expArea = 1;
+
+		yield return new WaitForSeconds(5.5f);
+
+		bomb.GetComponent<Bomb>().expAreaNorm = 3;
+		bombEx.GetComponent<BombEx>().expArea = 4;
+
+	}
+
 	IEnumerator SpeedDown (){
 
 		GameObject.Find("Player").GetComponent<PlayerMovement>().speed = 150f;
